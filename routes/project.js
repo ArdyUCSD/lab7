222c620ccffebdf446e4b4084a5af0ff3d842f51
vagrant <vagrant@precise32.(none)>
2015-02-15 05:20:46 +0000

    I did what was asked of me

--- routes/project.js
+++ routes/project.js
@@ -2,9 +2,9 @@ var models = require('../models');
 
 exports.projectInfo = function(req, res) { 
   var projectID = req.params.id;
-
-  // query for the specific project and
-  // call the following callback
+  models.Project
+  .find({"_id":projectID})
+  .exec(afterQuery);
 
   function afterQuery(err, projects) {
     if(err) console.log(err);
@@ -15,6 +15,23 @@ exports.projectInfo = function(req, res) { 
 exports.addProject = function(req, res) {
   var form_data = req.body;
   console.log(form_data);
+  var newPost = new models.Project ({
+    "title": form_data["project_title"],
+    "date": form_data["date"],
+    "image": form_data["image_url"],
+    "summary": form_data["summary"]
+  })
+  newPost.save(afterSaving);
+
+  function afterSaving(err) {
+    if(err) {console.log(err); res.send(500); }
+    res.redirect('/');
+  }
+
+  function addCallback(err,projects) {
+    if(err) console.log(err);
+    res.send(projects[0])
+  }
 
   // make a new Project and save it to the DB
   // YOU MUST send an OK response w/ res.send();
@@ -22,7 +39,13 @@ exports.addProject = function(req, res) {
 
 exports.deleteProject = function(req, res) {
   var projectID = req.params.id;
+  models.Project
+  .find({"_id":projectID})
+  .remove()
+  .exec(deleteCallback)
 
-  // find the project and remove it
-  // YOU MUST send an OK response w/ res.send();
+  function deleteCallback(err,projects) {
+    if(err) console.log(err);
+    res.send(projects[0])
+  }
 }
